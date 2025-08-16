1f47dd36d7c664108811402e835a212f25c18ce4
xiaokang00010 <zhouyongkang4321@gmail.com>
2025-08-16 21:50:52 +0800

    chores: add null functionality test to `code_test.py`

--- tools/code_test.py
+++ tools/code_test.py
@@ -23,6 +23,7 @@ TEST_CASES = {
     "test1": {"type": "library"},
     "worklist": {"type": "executable", "return_code": 78},
     "null-interface": {"type": "executable", "return_code": 0},
+    "null": {"type": "executable", "return_code": 0},
 }
 
 
