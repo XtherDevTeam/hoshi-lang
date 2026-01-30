afe06bb846afc9de7d01cc47183db05c000bbae3
xiaokang00010 <zhouyongkang4321@gmail.com>
2026-01-30 22:33:14 +0800

    bugfix: fix incompatiblity with fs.Path constructor when handling backslash as sep in Win32 platform

--- share/defines.h
+++ share/defines.h
@@ -1,7 +1,7 @@
 #ifndef HOSHI_LANG_DEFINES_H
 #define HOSHI_LANG_DEFINES_H
 
-#define HOSHI_LANG_VERSION "63"
-#define HOSHI_LANG_GIT_COMMIT_HASH "f6cf28"
+#define HOSHI_LANG_VERSION "64"
+#define HOSHI_LANG_GIT_COMMIT_HASH "acaea6"
 
 #endif
